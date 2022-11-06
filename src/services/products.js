ba986534fd92330f2b0fa5fd449741b8e627aa58
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-06 17:52:37 -0600

    feat: creating serviceHelpers and product services

---
+++ src/services/products.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import { BASE_URL } from '../utils/serviceHelpers'
+
+export const getAllProducts = () => {
+  return axios.get(BASE_URL)
+    .then(response => ({data: response.data, status: response.status}))
+    .catch(error => ({error: error.message, status: error.response.status}))
+}
