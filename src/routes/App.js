7eaa4d9a0699a441ba00ba007247821c87c14012
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-07 15:00:12 -0600

    feat: installing react router and setting up first routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import Home from '../pages/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
       <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>}/>
+          <Route path='*' element={<h1>Nothing here</h1>}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
