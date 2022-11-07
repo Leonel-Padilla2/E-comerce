eb89ee1b388ca8f0221cf1bcf84991a685986b8f
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-07 00:54:55 -0600

    feat: creating ProductCard component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,17 @@
+import './ProductCard.css'
+
+const ProductCard = ({productDetails = {}, className}) => {
+  const {title, image, category, price} = productDetails
+  console.log(productDetails)
+  return (
+    <div className={`product-card-container ${className}`}>
+      <img className='product-image' src={image} alt="product">
+      </img>
+      <h4 className='product-title'>{title}</h4>
+      <span className='product-category'>{category}</span>
+      <h2 className='product-price'>${price}</h2>
+    </div>
+  )
+}
+
+export default ProductCard
