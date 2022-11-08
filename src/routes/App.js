a1358a961f2f7310ea2a682c11dfbe257e92d399
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-07 23:25:13 -0600

    feat: adding ProductDetail page route.

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Home from '../pages/Home/Home';
+import ProductDetails from '../pages/ProductDetails/ProductDetails';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home/>}/>
+          <Route path='/product/:id' element={<ProductDetails/>}/>
           <Route path='*' element={<h1>Nothing here</h1>}/>
         </Routes>
       </BrowserRouter>
