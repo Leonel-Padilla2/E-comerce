71563c6d56548c6691d06ab247a6e84a50598601
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-07 23:14:57 -0600

    feat: adding selectedProduct in the initialState and in productReducer as well

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,13 +1,17 @@
 import {actionTypes} from '../constants/actionTypes'
 
 const initialState = {
-  products: []
+  products: [],
+  selectedProduct: {}
 }
 
 export const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.SET_PRODUCTS:
       return {...state, products: action.payload}
+
+      case actionTypes.SELECTED_PRODUCT:
+        return {...state, selectedProduct: action.payload}
 
     default: 
       return state
