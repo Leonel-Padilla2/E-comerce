e53d903a941a8035ad8e98230f2cf7d390208615
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-08 21:06:58 -0600

    feat: adding logo in Header component.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.css'
+import logo from '../../img/logo.png'
 
 const Header = () => {
   return (
     <div className='header-container'>
-      <h2>E-comerce</h2>
+      <a href="/">
+        <img className='header-logo' src={logo} alt={'logo'} />
+      </a>
+      <h3>E-commerce</h3>
     </div>
   )
 }
