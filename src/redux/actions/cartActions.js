2257bb0bfecca4c7870de8ba02feec3f4a5163f3
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-11 15:52:56 -0600

    feat: creating cart constants, actions and reducer.

---
+++ src/redux/actions/cartActions.js
@@ -0,0 +1,34 @@
+import { actionTypes } from "../constants/actionTypes"
+
+/*Verifies if the product is already added, if is, increase it's quantity once.*/
+export const addProductCart = () => (dispatch, getState) => {
+  const {selectedProduct} = getState().allProducts.allProducts
+  const {products} = getState().cart
+
+  if (products.find(product => product.id === selectedProduct.id)){
+    dispatch(increaseQuantityProductCart(selectedProduct.id))
+  } else {
+    dispatch({type: actionTypes.ADD_PRODUCT_CART, payload: selectedProduct})
+  }
+}
+
+export const removeProductCart = (id) => {
+  return{
+    type: actionTypes.ADD_PRODUCT_CART, 
+    payload: id
+  }
+}
+
+export const increaseQuantityProductCart = (id) => {
+  return{
+    type: actionTypes.INCREASE_QUANTITY_PRODUCT_CART,
+    payload: id
+  }
+}
+
+export const decreaseQuantityProductCart = (id) => {
+  return{
+    type: actionTypes.DECREASE_QUANTITY_PRODUCT_CART,
+    payload: id
+  }
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
+import { cartReducer } from "./cartReducer";
 import { fetchingReducer } from "./fetchingReducer";
-import {productReducer} from './productReducer'
+import { productReducer} from './productReducer'
 
 export const reducers = combineReducers({
   allProducts: combineReducers({
     allProducts: productReducer,
     status: fetchingReducer
-  })
+  }),
+  cart: cartReducer
 })
