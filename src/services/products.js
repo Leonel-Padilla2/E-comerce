55f8d687525fb88f31a5e531e6ebfb2a304e392c
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-10 20:55:44 -0600

    feat: handling error when the user does not have internet connection.

--- src/services/products.js
+++ src/services/products.js
@@ -4,11 +4,17 @@ import { BASE_URL } from '../utils/serviceHelpers'
 export const getAllProducts = () => {
   return axios.get(BASE_URL)
     .then(response => ({data: response.data, status: response.status}))
-    .catch(error => ({error: error.message, status: error.response.status}))
+    .catch(error => {
+      if(!error.status) return({error: 'Failed connection', status: 0})
+      return ({error: error.message, status: error.response.status})
+    })
 }
 
 export const getProductById = (id) => {
   return axios.get(`${BASE_URL}/${id}`)
     .then(response => ({data: response.data, status: response.status}))
-    .catch(error => ({error: error.message, status: error.response.status}))
+    .catch(error => {
+      if(!error.status) return({error: 'Failed connection', status: 0})
+      return ({error: error.message, status: error.response.status})
+    })
 }
