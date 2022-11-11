bbc431bcb0abc4550661093d2d3c5bc2b4500bf1
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-10 22:55:22 -0600

    fix: wrapping e-commers logo inside Link component instead of <a/> tag.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import './Header.css'
 import logo from '../../img/logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header-container'>
-      <a href="/">
+      <Link to={'/'}>
         <img className='header-logo' src={logo} alt={'logo'} />
-      </a>
+      </Link>
       <h3>E-commerce</h3>
     </div>
   )
