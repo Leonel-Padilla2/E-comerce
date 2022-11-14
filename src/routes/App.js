a9940ed6af17856cf4b522e6f71854276d508222
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-11-14 17:36:32 -0600

    feat: adding Cart page route.

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import Cart from '../pages/Cart/Cart';
 import Home from '../pages/Home/Home';
 import ProductDetails from '../pages/ProductDetails/ProductDetails';
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
         <Header/>
         <Routes>
           <Route index element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
           <Route path='/product/:id' element={<ProductDetails/>}/>
           <Route path='*' element={<h1>Nothing here</h1>}/>
         </Routes>
