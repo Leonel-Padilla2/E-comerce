940766c82991713584bea64404e6ce88c434345d
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-12-02 23:04:33 -0600

    feat: implementing LazyLoadImage component in ProductCard componente.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,5 +1,8 @@
-import { useNavigate } from 'react-router-dom'
 import './ProductCard.css'
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { useNavigate } from 'react-router-dom'
+
 
 const ProductCard = ({productDetails = {}, className}) => {
   const navigate = useNavigate()
@@ -11,7 +14,7 @@ const ProductCard = ({productDetails = {}, className}) => {
 
   return (
     <div onClick={handleOnClick} className={`product-card-container ${className}`}>
-      <img className='product-image' src={image} alt="product"/>
+      <LazyLoadImage effect='blur' width={'100%'} height={'60%'} className='product-image' src={image} alt={title}/>
       <h4 className='product-title'>{title}</h4>
       <span className='product-category'>{category}</span>
       <h2 className='product-price'>${price}</h2>
