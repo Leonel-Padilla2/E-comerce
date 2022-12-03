f39c5e7d64a02b74be2e657ec36af040f94e4832
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-12-02 23:05:34 -0600

    feat: implementing LazyLoadImage component in CartProductCard component.

--- src/components/CartProductCard/CartProductCard.jsx
+++ src/components/CartProductCard/CartProductCard.jsx
@@ -1,12 +1,15 @@
-import ProductOptions from '../ProductOptions/ProductOptions'
 import './CartProductCard.css'
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import ProductOptions from '../ProductOptions/ProductOptions'
+
 
 
 const CartProductCard = ({product, className}) => {
   const {image, title, price, quantity, id} = product
   return (
     <div className={`cart-product-card-container ${className}`}>
-      <img className='cart-product-image' src={image} alt="product" />
+      <LazyLoadImage className='cart-product-image' src={image} alt="product" effect='blur'/>
       <h3 className='cart-product-title'>{title}</h3>
       <ProductOptions quantity={quantity} productId={id}/>
       <h2 className='cart-product-price'>${price}</h2>
